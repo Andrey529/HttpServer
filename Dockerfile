0c8e18822a01b87113cec8b788d020844e14372b
Andrey529 <andr02eyes@gmail.com>
2023-04-27 22:12:15 +0300

    added docker support

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Build stage
+FROM gcc:latest as build
+WORKDIR /build
+
+ENV BUILD_TYPE=Debug
+
+# Install all packages
+RUN apt-get update && \
+    apt-get install -y cmake
+
+ADD ./src /app/src
+ADD ./CMakeLists.txt /app
+
+WORKDIR /app/build
+# Build binaries
+RUN cmake .. && \
+    cmake --build .
+
+ENTRYPOINT ["./HttpServer", "0.0.0.0", "8080"]
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include "Server.h"
 
-int main() {
-    http::Server server("127.0.0.1", 1234);
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cerr << "Incorrect number of input arguments. Must be\n"
+                     "<program-name> <ip-address> <port>" << std::endl;
+        return 1;
+    }
 
+    http::Server server(argv[1], std::stoi(argv[2]));
     server.start();
+
+    return 0;
 }
